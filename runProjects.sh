280ff37637eb68dcd7ee92754ca9bd0499a47773
Evan Farrer <Evan.R.Farrer@raytheon.com>
2012-03-18 19:18:29 -0600

    Display expected vs actual results when an answer is wrong.

--- runProjects.sh
+++ runProjects.sh
@@ -8,6 +8,9 @@ for dir in $(ls); do
         COUNT=$(echo "$dir" | sed 's/Euler//')
         if ! grep "^${COUNT}[.] ${ANSWER}" ./EulerSolutions.txt > /dev/null; then
             echo "It appears that Euler #${COUNT} is wrong."
+            echo "Got \"${COUNT}. $ANSWER\""
+            expecting=$(grep "^${COUNT}[.].*" ./EulerSolutions.txt | tr -d '\r')
+            echo "Expecting \"$expecting\""
             exit 1
         fi
     fi
